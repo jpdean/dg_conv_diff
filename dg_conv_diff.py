a7f78b99d81d05f6a3d730b6ed06b2a183531057
Joe Dean <jpd62@cam.ac.uk>
2022-10-04 17:29:41 +0100

    Change problem

--- dg_conv_diff.py
+++ dg_conv_diff.py
@@ -20,9 +20,9 @@ class TimeDependentExpression():
         return self.expression(x, self.t)
 
 
-n = 10
+n = 32
 k = 1
-t_end = 5.0
+t_end = 1.0
 num_time_steps = 32
 
 msh = mesh.create_unit_square(MPI.COMM_WORLD, n, n)
@@ -31,7 +31,7 @@ V = fem.FunctionSpace(msh, ("Discontinuous Lagrange", k))
 
 u, v = ufl.TrialFunction(V), ufl.TestFunction(V)
 u_n = fem.Function(V)
-# u_n.interpolate(lambda x: np.sin(np.pi * x[0]) * np.sin(np.pi * x[1]))
+u_n.interpolate(lambda x: np.sin(np.pi * x[0]) * np.sin(np.pi * x[1]))
 
 w = fem.Constant(msh, np.array([1.0, 0.0], dtype=PETSc.ScalarType))
 
@@ -44,7 +44,7 @@ alpha = fem.Constant(
 kappa = fem.Constant(msh, PETSc.ScalarType(0.01))
 
 u_D_expr = TimeDependentExpression(
-    lambda x, t: x[0] - (1 - np.exp(100 * x[0])) / (1 - np.exp(100)))
+    lambda x, t: np.sin(np.pi * t) * np.cos(np.pi * x[1]))
 u_D = fem.Function(V)
 u_D.interpolate(u_D_expr)
 
@@ -63,7 +63,7 @@ a = fem.form(inner(u / delta_t, v) * dx -
                       inner(grad(v), u * n) * ds +
                       (alpha / h) * inner(u, v) * ds))
 
-f = fem.Constant(msh, PETSc.ScalarType(1.0))
+f = fem.Constant(msh, PETSc.ScalarType(0.0))
 L = fem.form(inner(f + u_n / delta_t, v) * dx -
              inner((1 - lmbda) * dot(w, n) * u_D, v) * ds +
              kappa * (- inner(u_n * n, grad(v)) * ds +
